4955f7aaa6fc206e26213de5ce112f0ad0a7050c
haoranhaoshi <txwy123456@163.com>
2018-07-08 15:04:26 +0800

    aimaid
    aimaid

---
+++ AIMaid/app/src/main/java/com/example/administrator/aimaid/MaidActivity.java
@@ -0,0 +1,530 @@
+package com.example.administrator.aimaid;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.AudioManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.baidu.speech.EventListener;
+import com.baidu.speech.EventManager;
+import com.baidu.speech.EventManagerFactory;
+import com.baidu.speech.asr.SpeechConstant;
+import com.baidu.tts.auth.AuthInfo;
+import com.baidu.tts.chainofresponsibility.logger.LoggerProxy;
+import com.baidu.tts.client.SpeechSynthesizer;
+import com.baidu.tts.client.SpeechSynthesizerListener;
+import com.baidu.tts.client.TtsMode;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+//import com.baidu.android.voicedemo.util.AutoCheck;
+//import com.baidu.speech.recognizerdemo.R;
+
+public class MaidActivity extends AppCompatActivity implements EventListener{
+    //STW
+    protected TextView txtLog;
+    protected TextView txtResult;
+    protected Button btn;
+    protected Button stopBtn;
+    JSONObject jsonObject;
+    protected String final_result;
+
+    private String maidName = "亚丝娜";
+    private String DESC_TEXT = maidName + "正在等待召唤";
+
+    private EventManager asr;
+
+    private boolean logTime = true;
+
+    private boolean enableOffline = false; // 测试离线命令词，需要改成true
+
+    /**
+     * 测试参数填在这里
+     */
+    private void startSTW() {
+        Map<String, Object> params = new LinkedHashMap<String, Object>();
+        String event = null;
+        event = SpeechConstant.ASR_START; // 替换成测试的event
+
+        if (enableOffline) {
+            params.put(SpeechConstant.DECODER, 2);
+        }
+        params.put(SpeechConstant.ACCEPT_AUDIO_VOLUME, false);
+        // params.put(SpeechConstant.NLU, "enable");
+        // params.put(SpeechConstant.VAD_ENDPOINT_TIMEOUT, 0); // 长语音
+        // params.put(SpeechConstant.IN_FILE, "res:///com/baidu/android/voicedemo/16k_test.pcm");
+        // params.put(SpeechConstant.VAD, SpeechConstant.VAD_DNN);
+        // params.put(SpeechConstant.PROP ,20000);
+        // params.put(SpeechConstant.PID, 1537); // 中文输入法模型，有逗号
+        // 请先使用如‘在线识别’界面测试和生成识别参数。 params同ActivityRecog类中myRecognizer.start(params);
+
+        // 复制此段可以自动检测错误
+        /*(new AutoCheck(getApplicationContext(), new Handler() {
+            public void handleMessage(Message msg) {
+                if (msg.what == 100) {
+                    AutoCheck autoCheck = (AutoCheck) msg.obj;
+                    synchronized (autoCheck) {
+                        String message = autoCheck.obtainErrorMessage(); // autoCheck.obtainAllMessage();
+                        txtLog.append(message + "\n");
+                        ; // 可以用下面一行替代，在logcat中查看代码
+                        // Log.w("AutoCheckMessage", message);
+                    }
+                }
+            }
+        },enableOffline)).checkAsr(params);*/
+        String json = null; // 可以替换成自己的json
+        json = new JSONObject(params).toString(); // 这里可以替换成你需要测试的json
+        asr.send(event, json, null, 0, 0);
+        printLog("输入参数：" + json);
+    }
+
+    private void stop() {
+        stopSTW();
+        stopWTS();
+    }
+
+    private void stopSTW() {
+        printLog("停止识别：ASR_STOP");
+        asr.send(SpeechConstant.ASR_STOP, null, null, 0, 0);
+    }
+
+    private void stopWTS() {
+        txtLog.setText(DESC_TEXT);
+        print("停止合成引擎 按钮已经点击");
+        int result = mSpeechSynthesizer.stop();
+        checkResult(result, "stop");
+    }
+
+    private void loadOfflineEngine() {
+        Map<String, Object> params = new LinkedHashMap<String, Object>();
+        params.put(SpeechConstant.DECODER, 2);
+        params.put(SpeechConstant.ASR_OFFLINE_ENGINE_GRAMMER_FILE_PATH, "assets://baidu_speech_grammar.bsg");
+        asr.send(SpeechConstant.ASR_KWS_LOAD_ENGINE, new JSONObject(params).toString(), null, 0, 0);
+    }
+
+    private void unloadOfflineEngine() {
+        asr.send(SpeechConstant.ASR_KWS_UNLOAD_ENGINE, null, null, 0, 0); //
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maid);
+        initView();
+        initPermission();
+        asr = EventManagerFactory.create(this, "asr");
+        asr.registerListener(this); //  EventListener 中 onEvent方法
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtResult.setText("");
+                TEXT = maidName + "正在等待聊天";
+                txtLog.setText(TEXT);
+                speak();
+                isExit = false;
+            }
+        });
+        stopBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtResult.setText("");
+                TEXT = "主人记得再找我";
+                speak();
+                isExit = true;
+            }
+        });
+        if (enableOffline) {
+            loadOfflineEngine(); // 测试离线命令词请开启, 测试 ASR_OFFLINE_ENGINE_GRAMMER_FILE_PATH 参数时开启
+        }
+        initTTs();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //STW
+        asr.send(SpeechConstant.ASR_CANCEL, "{}", null, 0, 0);
+        if (enableOffline) {
+            unloadOfflineEngine(); // 测试离线命令词请开启, 测试 ASR_OFFLINE_ENGINE_GRAMMER_FILE_PATH 参数时开启
+        }
+        //WTS
+        if (mSpeechSynthesizer != null) {
+            mSpeechSynthesizer.stop();
+            mSpeechSynthesizer.release();
+            mSpeechSynthesizer = null;
+            print("释放资源成功");
+        }
+    }
+
+    protected boolean isExit;
+    //   EventListener  回调方法
+    @Override
+    public void onEvent(String name, String params, byte[] data, int offset, int length) {
+        String logTxt = "name: " + name;
+
+        if (params != null && !params.isEmpty()) {
+            logTxt += " ;params :" + params;
+        }
+        if (name.equals("asr.partial")) {
+            try {
+                jsonObject = new JSONObject(params);
+                final_result = jsonObject.getString("best_result");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        if (name.equals("asr.finish")) {
+            if(final_result.equals("")){
+                return;
+            }
+            switch (final_result){
+                case "我做了什么":
+                    TEXT = "主人创造了我，使我能感受全新的世界";
+                    break;
+                case "我怎么样":
+                    TEXT = "主人很好，和我一样喜欢学习";
+                    break;
+                case "你怎么样":
+                    TEXT = "我很乖，喜欢一直陪伴主人";
+                    break;
+                case "我的家人怎么样":
+                    TEXT = "他们像你爱他们一样，深深地爱着你";
+                    break;
+                case "我能做什么":
+                    TEXT = "广阔天地，大有作为，主人加油";
+                    break;
+                case "解除连接":
+                    txtResult.setText("");
+                    TEXT = "那主人记得下次找我聊天";
+                    isExit = true;
+                    break;
+                default:
+                    TEXT = maidName + "不知道怎么回答主人，好伤心";
+                    break;
+            }
+            stopSTW();
+            txtResult.setText(final_result);
+            txtLog.append("主人：" + final_result + "\n");
+            final_result = "";
+            new Handler().postDelayed(new Runnable(){
+                public void run() {
+                    speak();
+                }
+            }, 500);
+        }
+        printLog(logTxt);
+    }
+
+    private void printLog(String text) {
+        if (logTime) {
+            text += "  ;time=" + System.currentTimeMillis();
+        }
+        text += "\n";
+        Log.i(getClass().getName(), text);
+    }
+
+
+    private void initView() {
+        txtResult = (TextView) findViewById(R.id.txtResult);
+        txtLog = (TextView) findViewById(R.id.txtLog);
+        btn = (Button) findViewById(R.id.btn);
+        stopBtn = (Button) findViewById(R.id.btn_stop);
+        txtLog.setText(DESC_TEXT + "\n");
+    }
+
+    /**
+     * android 6.0 以上需要动态申请权限
+     */
+    private void initPermission() {
+        String permissions[] = {
+                Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.ACCESS_NETWORK_STATE,
+                Manifest.permission.INTERNET,
+                Manifest.permission.READ_PHONE_STATE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.INTERNET,
+                Manifest.permission.ACCESS_NETWORK_STATE,
+                Manifest.permission.MODIFY_AUDIO_SETTINGS,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_SETTINGS,
+                Manifest.permission.READ_PHONE_STATE,
+                Manifest.permission.ACCESS_WIFI_STATE,
+                Manifest.permission.CHANGE_WIFI_STATE
+        };
+
+        ArrayList<String> toApplyList = new ArrayList<String>();
+
+        for (String perm : permissions) {
+            if (PackageManager.PERMISSION_GRANTED != ContextCompat.checkSelfPermission(this, perm)) {
+                toApplyList.add(perm);
+                // 进入到这里代表没有权限.
+
+            }
+        }
+        String tmpList[] = new String[toApplyList.size()];
+        if (!toApplyList.isEmpty()) {
+            ActivityCompat.requestPermissions(this, toApplyList.toArray(tmpList), 123);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        // 此处为android 6.0以上动态授权的回调，用户自行实现。
+    }
+
+    //WTS
+    private static String TEXT = "待读";
+
+    // ================== 初始化参数设置开始 ==========================
+    /**
+     * 发布时请替换成自己申请的appId appKey 和 secretKey。注意如果需要离线合成功能,请在您申请的应用中填写包名。
+     * 本demo的包名是com.baidu.tts.sample，定义在build.gradle中。
+     */
+    protected String appId = "11005757";
+
+    protected String appKey = "Ovcz19MGzIKoDDb3IsFFncG1";
+
+    protected String secretKey = "e72ebb6d43387fc7f85205ca7e6706e2";
+
+    // TtsMode.MIX; 离在线融合，在线优先； TtsMode.ONLINE 纯在线； 没有纯离线
+    private TtsMode ttsMode = TtsMode.ONLINE;
+
+    // ================选择TtsMode.ONLINE  不需要设置以下参数; 选择TtsMode.MIX 需要设置下面2个离线资源文件的路径
+    private static final String TEMP_DIR = "/sdcard/baiduTTS"; // 重要！请手动将assets目录下的3个dat 文件复制到该目录
+
+    // 请确保该PATH下有这个文件
+    private static final String TEXT_FILENAME = TEMP_DIR + "/" + "bd_etts_text.dat";
+
+    // 请确保该PATH下有这个文件 ，m15是离线男声
+    private static final String MODEL_FILENAME =
+            TEMP_DIR + "/" + "bd_etts_common_speech_m15_mand_eng_high_am-mix_v3.0.0_20170505.dat";
+
+    // ===============初始化参数设置完毕，更多合成参数请至getParams()方法中设置 =================
+
+    protected SpeechSynthesizer mSpeechSynthesizer;
+
+    protected Handler mainHandler;
+
+    private static final String TAG = "WTSActivity";
+    /**
+     * 注意此处为了说明流程，故意在UI线程中调用。
+     * 实际集成中，该方法一定在新线程中调用，并且该线程不能结束。具体可以参考NonBlockSyntherizer的写法
+     */
+    private void initTTs() {
+        LoggerProxy.printable(true); // 日志打印在logcat中
+        boolean isMix = ttsMode.equals(TtsMode.MIX);
+        boolean isSuccess;
+        if (isMix) {
+            // 检查2个离线资源是否可读
+            isSuccess = checkOfflineResources();
+            if (!isSuccess) {
+                return;
+            } else {
+                print("离线资源存在并且可读, 目录：" + TEMP_DIR);
+            }
+        }
+
+        mainHandler = new Handler() {
+            /*
+             * @param msg
+             */
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                if (msg.obj != null) {
+                    print(msg.obj.toString());
+                }
+            }
+
+        };
+        // 日志更新在UI中，可以换成MessageListener，在logcat中查看日志
+        SpeechSynthesizerListener listener = new UiMessageListener(mainHandler);
+
+        // 1. 获取实例
+        mSpeechSynthesizer = SpeechSynthesizer.getInstance();
+        mSpeechSynthesizer.setContext(this);
+
+        // 2. 设置listener
+        mSpeechSynthesizer.setSpeechSynthesizerListener(listener);
+
+        // 3. 设置appId，appKey.secretKey
+        int result = mSpeechSynthesizer.setAppId(appId);
+        checkResult(result, "setAppId");
+        result = mSpeechSynthesizer.setApiKey(appKey, secretKey);
+        checkResult(result, "setApiKey");
+
+        // 4. 支持离线的话，需要设置离线模型
+        if (isMix) {
+            // 检查离线授权文件是否下载成功，离线授权文件联网时SDK自动下载管理，有效期3年，3年后的最后一个月自动更新。
+            isSuccess = checkAuth();
+            if (!isSuccess) {
+                return;
+            }
+            // 文本模型文件路径 (离线引擎使用)， 注意TEXT_FILENAME必须存在并且可读
+            mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_TTS_TEXT_MODEL_FILE, TEXT_FILENAME);
+            // 声学模型文件路径 (离线引擎使用)， 注意TEXT_FILENAME必须存在并且可读
+            mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_TTS_SPEECH_MODEL_FILE, MODEL_FILENAME);
+        }
+
+        // 5. 以下setParam 参数选填。不填写则默认值生效
+        // 设置在线发声音人： 0 普通女声（默认） 1 普通男声 2 特别男声 3 情感男声<度逍遥> 4 情感儿童声<度丫丫>
+        mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_SPEAKER, "4");
+        // 设置合成的音量，0-9 ，默认 5
+        mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_VOLUME, "9");
+        // 设置合成的语速，0-9 ，默认 5
+        mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_SPEED, "5");
+        // 设置合成的语调，0-9 ，默认 5
+        mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_PITCH, "5");
+
+        mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_MIX_MODE, SpeechSynthesizer.MIX_MODE_DEFAULT);
+        // 该参数设置为TtsMode.MIX生效。即纯在线模式不生效。
+        // MIX_MODE_DEFAULT 默认 ，wifi状态下使用在线，非wifi离线。在线状态下，请求超时6s自动转离线
+        // MIX_MODE_HIGH_SPEED_SYNTHESIZE_WIFI wifi状态下使用在线，非wifi离线。在线状态下， 请求超时1.2s自动转离线
+        // MIX_MODE_HIGH_SPEED_NETWORK ， 3G 4G wifi状态下使用在线，其它状态离线。在线状态下，请求超时1.2s自动转离线
+        // MIX_MODE_HIGH_SPEED_SYNTHESIZE, 2G 3G 4G wifi状态下使用在线，其它状态离线。在线状态下，请求超时1.2s自动转离线
+
+        mSpeechSynthesizer.setAudioStreamType(AudioManager.MODE_IN_CALL);
+        // 不使用压缩传输
+        // mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_AUDIO_ENCODE, SpeechSynthesizer.AUDIO_ENCODE_PCM);
+        // mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_AUDIO_RATE, SpeechSynthesizer.AUDIO_BITRATE_PCM);
+
+        // x. 额外 ： 自动so文件是否复制正确及上面设置的参数
+        Map<String, String> params = new HashMap<>();
+        // 复制下上面的 mSpeechSynthesizer.setParam参数
+        // 上线时请删除AutoCheck的调用
+        if (isMix) {
+            params.put(SpeechSynthesizer.PARAM_TTS_TEXT_MODEL_FILE, TEXT_FILENAME);
+            params.put(SpeechSynthesizer.PARAM_TTS_SPEECH_MODEL_FILE, MODEL_FILENAME);
+        }
+        InitConfig initConfig =  new InitConfig(appId, appKey, secretKey, ttsMode, params, listener);
+        /*AutoCheck.getInstance(getApplicationContext()).check(initConfig, new Handler() {
+            @Override
+            *//**
+         * 开新线程检查，成功后回调
+         *//*
+            public void handleMessage(Message msg) {
+                if (msg.what == 100) {
+                    AutoCheck autoCheck = (AutoCheck) msg.obj;
+                    synchronized (autoCheck) {
+                        String message = autoCheck.obtainDebugMessage();
+                        print(message); // 可以用下面一行替代，在logcat中查看代码
+                        // Log.w("AutoCheckMessage", message);
+                    }
+                }
+            }
+
+        });*/
+
+        // 6. 初始化
+        result = mSpeechSynthesizer.initTts(ttsMode);
+        checkResult(result, "initTts");
+
+    }
+
+    /**
+     * 检查appId ak sk 是否填写正确，另外检查官网应用内设置的包名是否与运行时的包名一致。本demo的包名定义在build.gradle文件中
+     *
+     * @return
+     */
+    private boolean checkAuth() {
+        AuthInfo authInfo = mSpeechSynthesizer.auth(ttsMode);
+        if (!authInfo.isSuccess()) {
+            // 离线授权需要网站上的应用填写包名。本demo的包名是com.baidu.tts.sample，定义在build.gradle中
+            String errorMsg = authInfo.getTtsError().getDetailMessage();
+            print("【error】鉴权失败 errorMsg=" + errorMsg);
+            return false;
+        } else {
+            print("验证通过，离线正式授权文件存在。");
+            return true;
+        }
+    }
+
+    /**
+     * 检查 TEXT_FILENAME, MODEL_FILENAME 这2个文件是否存在，不存在请自行从assets目录里手动复制
+     *
+     * @return
+     */
+    private boolean checkOfflineResources() {
+        String[] filenames = {TEXT_FILENAME, MODEL_FILENAME};
+        for (String path : filenames) {
+            File f = new File(path);
+            if (!f.canRead()) {
+                print("[ERROR] 文件不存在或者不可读取，请从assets目录复制同名文件到：" + path);
+                print("[ERROR] 初始化失败！！！");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void speak() {
+        /* 以下参数每次合成时都可以修改
+         *  mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_SPEAKER, "0");
+         *  设置在线发声音人： 0 普通女声（默认） 1 普通男声 2 特别男声 3 情感男声<度逍遥> 4 情感儿童声<度丫丫>
+         *  mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_VOLUME, "5"); 设置合成的音量，0-9 ，默认 5
+         *  mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_SPEED, "5"); 设置合成的语速，0-9 ，默认 5
+         *  mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_PITCH, "5"); 设置合成的语调，0-9 ，默认 5
+         *
+         *  mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_MIX_MODE, SpeechSynthesizer.MIX_MODE_DEFAULT);
+         *  MIX_MODE_DEFAULT 默认 ，wifi状态下使用在线，非wifi离线。在线状态下，请求超时6s自动转离线
+         *  MIX_MODE_HIGH_SPEED_SYNTHESIZE_WIFI wifi状态下使用在线，非wifi离线。在线状态下， 请求超时1.2s自动转离线
+         *  MIX_MODE_HIGH_SPEED_NETWORK ， 3G 4G wifi状态下使用在线，其它状态离线。在线状态下，请求超时1.2s自动转离线
+         *  MIX_MODE_HIGH_SPEED_SYNTHESIZE, 2G 3G 4G wifi状态下使用在线，其它状态离线。在线状态下，请求超时1.2s自动转离线
+         */
+
+        if (mSpeechSynthesizer == null) {
+            print("[ERROR], 初始化失败");
+            return;
+        }
+        int WTSTime = TEXT.length() * 300;
+        if(isExit){
+            new Handler().postDelayed(new Runnable(){
+                public void run() {
+                    stopWTS();
+                    return;
+                }
+            }, WTSTime);
+        }else{
+
+            new Handler().postDelayed(new Runnable(){
+                public void run() {
+                    startSTW();
+                    txtResult.setText(TEXT);
+                    txtLog.append(maidName + "：" + TEXT + "\n");
+                }
+            }, WTSTime);
+        }
+        int result = mSpeechSynthesizer.speak(TEXT);
+
+        print("合成并播放 按钮已经点击");
+        checkResult(result, "speak");
+    }
+
+    private void print(String message) {
+        Log.i(TAG, message);
+    }
+
+    private void checkResult(int result, String method) {
+        if (result != 0) {
+            print("error code :" + result + " method:" + method + ", 错误码文档:http://yuyin.baidu.com/docs/tts/122 ");
+        }
+    }
+
+}
